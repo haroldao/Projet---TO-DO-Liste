657d9f61d06c65a366e37951c84ae4d55c8524ff
haroldao <harold86340@gmail.com>
2020-12-04 17:15:22 +0100

    🚧TEST - Application dans console étape 1

--- todo.py
+++ todo.py
@@ -4,6 +4,11 @@ import sqlite3 as sql
 from prettytable import from_db_cursor
 
 
+def app():
+    user_selection = input(
+        "1. C - Créer une nouvelle tâche\n2. R - Consulter la liste des tâches non-terminée, terminée, urgentes\n3. U - Modifier l'état d'une tâche\n4. D - Supprimer une tâche\n5. Quitter l'application")
+
+
 def create_table():
     connection = sql.connect("todo.db")
     cursor = connection.cursor()
