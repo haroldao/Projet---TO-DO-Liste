2c19f9fb75c9e5ed57fb6689e431d60a21a7493f
haroldao <harold86340@gmail.com>
2020-12-04 17:01:16 +0100

    Creation de la function create_table (connection + cursor)

--- todo.py
+++ todo.py
@@ -2,3 +2,9 @@
 import sqlite3 as sql
 # Import de PrettyTable (affichage du tableau dans la console)
 from prettytable import from_db_cursor
+
+
+def create_table():
+    connection = sql.connect("todo.db")
+    cursor = connection.cursor()
+    
