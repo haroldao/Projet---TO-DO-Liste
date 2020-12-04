da9b3498c12cbeaaf603bfb01ebaa0fda665fd87
haroldao <harold86340@gmail.com>
2020-12-04 17:05:48 +0100

    Fin creation function create_table

--- todo.py
+++ todo.py
@@ -7,4 +7,15 @@ from prettytable import from_db_cursor
 def create_table():
     connection = sql.connect("todo.db")
     cursor = connection.cursor()
-    
+
+    query = '''
+        CREATE TABLE IF NOT EXISTS todo(
+            id INTEGER PRIMARY KEY,
+            designation TEXT,
+            echeance TEXT,
+            etat TEXT
+        )
+    '''
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
